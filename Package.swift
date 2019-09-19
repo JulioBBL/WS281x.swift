fdcb8b5c914349fe5adb9b6441a79489483c6045
Julio Brazil <julio.lopes@btgpactual.com>
2019-09-19 18:06:58 -0300

    upgraded to swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,17 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "WS281x",
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 majorVersion: 1)
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "WS281x",
+            dependencies: ["SwiftyGPIO"],
+            path: "Sources"
+        )
     ]
 )
